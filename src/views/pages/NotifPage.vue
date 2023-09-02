c8809cb3b2391cd617238c2f706d396fc5a67ee3
Alina <AliKochneva@yandex.ru>
2023-09-02 19:11:06 +0700

    notifications

--- src/views/pages/NotifPage.vue
+++ src/views/pages/NotifPage.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="center">
         <h2>Страница уведомлений</h2>
+        <div v-for="note in notifications" :key=note.id>
+            <span># {{ note.id }}</span>
+            <p>{{ note.text }}</p>
+        </div>
         <button @click="goBack">Назад</button>
     </div>
 </template>
@@ -9,6 +13,24 @@
 <script>
 export default{
     name: 'NotificationPage',
+    data() {
+        return {
+            notifications: [
+                {
+                    id: 1,
+                    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, veritatis?'
+                },
+                {
+                    id: 2,
+                    text: 'Lsit amet consectetur adipisicing elit. Reiciendis, veritatis?'
+                },
+                {
+                    id: 3,
+                    text: 'Lorem ing elit. Reiciendis, veritatis?'
+                },
+            ]
+        }
+    },
     methods: {
         goBack(){
             this.$router.go(-1)
