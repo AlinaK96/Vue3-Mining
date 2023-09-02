fe6f17dfbd053e8cfe350dc3d0cbbe43b574dc23
Alina <AliKochneva@yandex.ru>
2023-09-02 11:53:04 +0700

    отображение даты и времени

--- src/components/SidebarComp.vue
+++ src/components/SidebarComp.vue
@@ -1,9 +1,46 @@
 <template>
-    <section className='sidebar'>
-        
+    <section class='sidebar'>
+        <div className='sidebar__header'>
+            <div class="header__data">
+                <p id="timestamp">{{dataBlock}}</p>
+                <span>{{timeBlock}}</span>
+            </div>
+            <div class="exit" title="Выход" @click="exit">
+                <button>Выход</button>
+                <img src="../assets/img/svg/exit.svg" alt="Упс..">
+            </div>
+            
 
+        </div>
     </section>
 </template>
+
+<script>
+export default {
+    data(){
+        return{
+            dataBlock: '',
+            timeBlock: ''
+        }
+    },        
+    created() {
+            setInterval(this.getNow, 1000);
+        },
+    methods: {
+        exit(){
+            this.$router.push('/')
+        },
+
+        getNow() {
+            let today = new Date();
+            let date = today.getDate()+'.'+ (today.getMonth()+1)+'.' + today.getFullYear();
+            let time = today.getHours() + ":" + today.getMinutes();
+            this.dataBlock = date
+            this.timeBlock = time;
+        }
+    },
+}
+</script>
 
 <style lang="css">
 
